64d6eeee90a83f72217471fe83d125649cb199e4
Nick Partridge <nicholas.partridge@elastic.co>
2022-06-15 11:34:58 -0700

    fix: report merge issues (#5)
    * testing fix
    
    * chore: upgrade playwright dev dep
    
    * fix: remaining edge case issues
    
    - only process data when found (i.e. on test error)
    - use regex to reuse index.html and not dig up html from pw-core
    - remove trace viewer for now
    
    * chore: update build dist files
    
    * fix: merge duplicate/sharded file reports
    
    * chore: update build dist files
    
    * chore: remove copied file
    
    * chore: remove temp dist directory

--- src/merge-reports.ts
+++ src/merge-reports.ts
@@ -14,13 +14,13 @@
  * @author Anoop Raveendran
  */
 
-import { createWriteStream } from "fs";
-import { mkdir, readFile, readdir, copyFile, appendFile } from "fs/promises";
+import { createWriteStream, existsSync } from "fs";
+import { mkdir, readFile, readdir, copyFile, appendFile, writeFile } from "fs/promises";
 import path from "path";
 import JSZip from "jszip";
 import yazl from "yazl";
 import Base64Encoder from "./Base64Encoder";
-import type { HTMLReport, Config, ZipDataFile } from "./types";
+import type { HTMLReport, Config, ZipDataFile, Stats, FileReport } from "./types";
 
 const SHOW_DEBUG_MESSAGES = false;
 
@@ -28,6 +28,8 @@ const defaultConfig = {
   outputFolderName: "merged-html-report",
   outputBasePath: process.cwd()
 }
+
+const re = /<script>\nwindow\.playwrightReportBase64\s=\s"data:application\/zip;base64,(.+)<\/script>/
 
 async function mergeHTMLReports(inputReportPaths: string[], givenConfig: Config = {}) {
   if(!Array.isArray(inputReportPaths) || inputReportPaths.length < 1) {
@@ -37,28 +39,23 @@ async function mergeHTMLReports(inputReportPaths: string[], givenConfig: Config 
 
   // Merge config with default values
   const { outputFolderName, outputBasePath } = {...defaultConfig, ...givenConfig};
-
-  const b64StartSearchStr = `window.playwrightReportBase64 = "data:application/zip;base64,`;
-  const b64EndSearchStr = `";</script>`;
-
   const outputPath = path.resolve(outputBasePath, `./${outputFolderName}`);
-
-  await mkdir(outputPath + "/trace", { recursive: true });
-  await mkdir(outputPath + "/data", { recursive: true });
 
   let mergedZipContent = new yazl.ZipFile();
   let aggregateReportJson: HTMLReport | null = null;
+  let baseIndexHtml = '';
+  const fileReportMap = new Map<string, FileReport>();
 
   for (let reportDir of inputReportPaths) {
     console.log(`Processing "${reportDir}"`)
     const indexHTMLContent = await readFile(reportDir + "/index.html", "utf8");
-    const startIdx = indexHTMLContent.indexOf(b64StartSearchStr);
-    const endIdx = indexHTMLContent.indexOf(b64EndSearchStr);
+    const [, base64Str] = re.exec(indexHTMLContent) ?? [];
 
-    const base64Str = indexHTMLContent.substring(
-      startIdx + b64StartSearchStr.length,
-      endIdx
-    );
+    if (!base64Str) throw new Error('index.html does not contain report data');
+
+    if (!baseIndexHtml) {
+      baseIndexHtml = indexHTMLContent.replace(re, '');
+    }
 
     const zipData = Buffer.from(base64Str, "base64").toString("binary");
     const zipFile = await JSZip.loadAsync(zipData);
@@ -71,60 +68,80 @@ async function mergeHTMLReports(inputReportPaths: string[], givenConfig: Config 
     await Promise.all(zipDataFiles.map(async ({ relativePath, file }) => {
       const fileContentString = await file.async("string");
       if (relativePath !== "report.json") {
-        mergedZipContent.addBuffer(
-          Buffer.from(fileContentString),
-          relativePath
-        );
+        const fileReportJson = JSON.parse(fileContentString) as FileReport;
+
+        if (fileReportMap.has(relativePath)) {
+          if (SHOW_DEBUG_MESSAGES) {
+            console.log('Merging duplicate file report: ' + relativePath);
+          }
+
+          const existingReport = fileReportMap.get(relativePath);
+
+          if (existingReport?.fileId !== fileReportJson.fileId) throw new Error('Error: collision with file ids in two file reports')
+
+          existingReport.tests.push(...fileReportJson.tests);
+        } else {
+          fileReportMap.set(relativePath, fileReportJson);
+        }
+
       } else {
         const currentReportJson = JSON.parse(fileContentString);
         if (SHOW_DEBUG_MESSAGES) {
+          console.log('---------- report.json ----------');
           console.log(JSON.stringify(currentReportJson, null, 2));
         }
         if (!aggregateReportJson) aggregateReportJson = currentReportJson;
         else {
-          aggregateReportJson.files.push(...currentReportJson.files);
-          Object.keys(aggregateReportJson.stats).forEach((key) => {
-            aggregateReportJson!.stats[key] += currentReportJson.stats[key];
-          });
-          aggregateReportJson.projectNames = [
-            ...new Set([
-              ...aggregateReportJson.projectNames,
-              ...currentReportJson.projectNames
-            ])
-          ];
+          currentReportJson.files.forEach((file) => {
+            const existingGroup = aggregateReportJson?.files.find(({ fileId }) => fileId === file.fileId);
+
+            if (existingGroup) {
+              existingGroup.tests.push(...file.tests);
+              mergeStats(existingGroup.stats, file.stats);
+            } else {
+              aggregateReportJson?.files.push(file);
+            }
+          })
+
+         mergeStats(aggregateReportJson.stats, currentReportJson.stats);
+
+         aggregateReportJson.projectNames = [
+           ...new Set([
+             ...aggregateReportJson.projectNames,
+             ...currentReportJson.projectNames
+           ])
+         ];
         }
       }
     }));
 
     const contentFolderName = "data";
     const contentFolderPath = `${reportDir}/${contentFolderName}/`;
-    let contentFiles = [];
-    
-    try {
-      await readdir(contentFolderPath);
-    } catch (e) {
-      // No-op
-    }
 
-    await Promise.all(
-      contentFiles.map(async (fileName) => {
-        const srcPath = path.resolve(
-          process.cwd(),
-          `${contentFolderPath}${fileName}`
-        );
-        const destPath = `${outputPath}/${contentFolderName}/${fileName}`;
-        if (SHOW_DEBUG_MESSAGES) {
-          console.log({
-            srcPath,
-            destPath
-          });
-        }
-        await copyFile(srcPath, destPath);
-        if (SHOW_DEBUG_MESSAGES) {
-          console.log(fileName);
-        }
-      })
-    );
+    if (existsSync(contentFolderPath)) {
+      await mkdir(outputPath + "/data", { recursive: true });
+      const contentFiles = await readdir(contentFolderPath);
+
+      await Promise.all(
+        contentFiles.map(async (fileName) => {
+          const srcPath = path.resolve(
+            process.cwd(),
+            `${contentFolderPath}${fileName}`
+          );
+          const destPath = `${outputPath}/${contentFolderName}/${fileName}`;
+          if (SHOW_DEBUG_MESSAGES) {
+            console.log({
+              srcPath,
+              destPath
+            });
+          }
+          await copyFile(srcPath, destPath);
+          if (SHOW_DEBUG_MESSAGES) {
+            console.log(fileName);
+          }
+        })
+      );
+    }
 
     if (SHOW_DEBUG_MESSAGES) {
       console.log({
@@ -133,61 +150,52 @@ async function mergeHTMLReports(inputReportPaths: string[], givenConfig: Config 
     }
   }
 
+  if (!baseIndexHtml) throw new Error('Base report index.html not found');
+
+  fileReportMap.forEach((fileReport, relativePath) => {
+     mergedZipContent.addBuffer(
+       Buffer.from(JSON.stringify(fileReport)),
+       relativePath,
+     );
+   })
+
   mergedZipContent.addBuffer(
     Buffer.from(JSON.stringify(aggregateReportJson)),
     "report.json"
   );
 
   if (SHOW_DEBUG_MESSAGES) {
+    console.log('---------- aggregateReportJson ----------');
     console.log(JSON.stringify(aggregateReportJson, null, 2));
   }
 
-  const appFolder = path.join(
-    path.dirname(require.resolve("playwright-core")),
-    "lib",
-    "webpack",
-    "htmlReport"
-  );
-  await copyFile(
-    path.join(appFolder, "index.html"),
-    path.join(outputPath, "index.html")
-  );
-
-  const indexFile = path.join(outputPath, "index.html");
-  await appendFile(
-    indexFile,
-    '<script>\nwindow.playwrightReportBase64 = "data:application/zip;base64,'
-  );
+  const indexFilePath = path.join(outputPath, "index.html");
+  await writeFile(indexFilePath, baseIndexHtml + `<script>
+window.playwrightReportBase64 = "data:application/zip;base64,`)
 
   await new Promise((f) => {
     mergedZipContent.end(undefined, () => {
       mergedZipContent.outputStream
         .pipe(new Base64Encoder())
-        .pipe(createWriteStream(indexFile, { flags: "a" }))
+        .pipe(createWriteStream(indexFilePath, { flags: "a" }))
         .on("close", f);
     });
   });
 
-  await appendFile(indexFile, '";</script>');
-
-  const traceViewerFolder = path.join(
-    path.dirname(require.resolve("playwright-core")),
-    "lib",
-    "webpack",
-    "traceViewer"
-  );
-  const traceViewerTargetFolder = path.join(outputPath, "trace");
-  await mkdir(traceViewerTargetFolder, { recursive: true });
-  for (const file of await readdir(traceViewerFolder)) {
-    if (file.endsWith(".map")) continue;
-    await copyFile(
-      path.join(traceViewerFolder, file),
-      path.join(traceViewerTargetFolder, file)
-    );
-  }
-  console.log("Merged successfully")
+  await appendFile(indexFilePath, '";</script>');
+  console.log(`Successfully merged ${inputReportPaths.length} report${inputReportPaths.length === 1 ? '' : 's'}`);
 }
 
 export {
   mergeHTMLReports
+}
+
+function mergeStats(base: Stats, added: Stats) {
+  base.total += added.total;
+  base.expected += added.expected;
+  base.unexpected += added.unexpected;
+  base.flaky += added.flaky;
+  base.skipped += added.skipped;
+  base.duration += added.duration;
+  base.ok = base.ok && added.ok;
 }
