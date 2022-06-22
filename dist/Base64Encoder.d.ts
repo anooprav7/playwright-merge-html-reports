06846c38494aa8df6056054051a2ad8432f4f5ff
nickofthyme <nicholas.partridge@elastic.co>
2022-06-21 20:30:26 -0700

    chore: remove test files

--- dist/Base64Encoder.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { Transform, TransformCallback } from 'stream';
-declare class Base64Encoder extends Transform {
-    private _remainder;
-    _transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback): void;
-    _flush(callback: TransformCallback): void;
-}
-export default Base64Encoder;
